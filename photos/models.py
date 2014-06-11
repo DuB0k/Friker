ce9c31b14bf01721a510d1e8362aa3a215783bd9
Pablo Castellanos <dubok@MacBookAir.local>
2014-06-11 11:12:59 +0200

    create detail photo template

--- photos/models.py
+++ photos/models.py
@@ -1,4 +1,7 @@
+# -*- coding utf-8 -*-
 from django.db import models
+from django.contrib.auth.models import User
+
 
 LICENSES = (
     ('RIG', 'Copyright'),
@@ -6,15 +9,24 @@ LICENSES = (
     ('CC', 'Creative Commons')
 )
 
+VISIBILITY_PUBLIC = 'PUB'
+VISIBILITY_PRIVATE = 'PRI'
+
+VISIBILITY = (
+    ('PUB', 'Publica'),
+    ('PRI', 'Privada'),
+)
+
 
 class Photo(models.Model):
-
+    owner = models.ForeignKey(User)
     name = models.CharField(max_length=150)
     url = models.URLField()
     description = models.TextField(blank=True) #opcional
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now_add=True, auto_now=True)
     license = models.CharField(max_length=3, choices=LICENSES)
+    visibility = models.CharField(max_length=3, choices=VISIBILITY)
 
     def __unicode__(self):
         return self.name
--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from models import Photo
+from models import Photo, VISIBILITY_PUBLIC
+from django.http.response import HttpResponseNotFound
 
 def home(request):
     """
@@ -7,10 +8,30 @@ def home(request):
     :param request: objeto request
     :return: objeto response
     """
-    photo_list = Photo.objects.all() #cogemos todas las fotos de la BD
+
+    photo_list = Photo.objects.filter(visibility=VISIBILITY_PUBLIC).order_by('-created_at') #cogemos todas las fotos de la BD
 
     context = {
-        'photos' : photo_list
+        'photos' : photo_list[:3] #Cogemos solo las 3 primeras fotos
     }
 
     return render(request, 'photos/index.html', context)
+
+
+def photo_detail(request, pk):
+    """
+    Muestra el detalle de una foto
+    :param request: objeto request
+    :param pk: primary key de la foto
+    :return: objeto response
+    """
+    possible_photos = Photo.objects.filter(pk=pk, visibility=VISIBILITY_PUBLIC)
+
+    if len(possible_photos) == 0:
+        return HttpResponseNotFound('No existe la foto seleccionada')
+    else:
+        context = {
+            'photo': possible_photos[0]
+        }
+
+        return render(request, 'photos/photo_detail.html', context)
