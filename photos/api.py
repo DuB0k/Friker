d1a6ef955f86be4540e24a44b0eec115e4b1a55b
Pablo Castellanos <dubok@MacBookAir.local>
2014-06-12 12:51:22 +0200

    Added REST DELETE api

--- photos/api.py
+++ photos/api.py
@@ -3,10 +3,12 @@ from django.contrib.auth.models import User
 from serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 
 
 class UserListAPI(APIView):
-
+    #Una lista de usuarios
     def get(self, request):
         # Recuperamos todos los usuarios del modelo
         users = User.objects.all()
@@ -22,7 +24,32 @@ class UserListAPI(APIView):
         serializer = UserSerializer(data=request.DATA)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=201)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             # Algo ha ido mal
-            return Response(serializer.errors, status=400)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserDetailAPI(APIView):
+    #Un unico usuario
+    def get(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        # Serializamos el user
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user, data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            # Algo ha ido mal
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
