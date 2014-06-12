1cba09db625c8e11fe2a1b7df2b0dcb1471bb007
Pablo Castellanos <dubok@MacBookAir.local>
2014-06-12 11:58:15 +0200

    Added first REST API POST

--- photos/api.py
+++ photos/api.py
@@ -10,8 +10,19 @@ class UserListAPI(APIView):
     def get(self, request):
         # Recuperamos todos los usuarios del modelo
         users = User.objects.all()
-        # Con el parametro many le decimos que le estamos pasasndo muchos usuarios
+        # Con el parametro many le decimos que le estamos pasando muchos usuarios
         # no solo un usuario
         serializer = UserSerializer(users, many=True)
         # El objeto data modifica el formato de los datos de forma transparente
         return Response(serializer.data)
+
+    def post(self, request):
+        # Para crear un usuario
+        # Hay que pasar request.DATA en vez de request.POST
+        serializer = UserSerializer(data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            # Algo ha ido mal
+            return Response(serializer.errors, status=400)
